7df0f21a6e2c62f35ebc78ac591300d7217e7dd3
LandgeRushikesh <rushilandge12@gmail.com>
2024-11-19 23:11:41 +0530

    data fetched

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let cityname = "London"
+let cityname = "Mumbai"
 let APIKey = `916aadbc8a2241c004d71a5301fdcda5`
 let url =  `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`
 
+
+let resTemp = document.querySelector(".res-temp")
+let reshumidity = document.querySelector(".res-humidity")
+let resWind = document.querySelector(".res-wind") 
+let city = document.querySelector(".city") 
 let fun = async() =>{
     let response = await fetch(url)
-    let data = response.json()
+    let data = await response.json()
     console.log(data)
+    let temp = data.main.temp
+    let wind = data.wind.speed
+    let humidity = data.main.humidity
+    cityname = data.name
+
+    resTemp.innerText = `${temp}°C`
+    city.innerText = cityname
+    reshumidity.innerText = `humidity : ${humidity}%`
+    resWind.innerText = `${wind} m/s`
 }
 
 fun()
+
