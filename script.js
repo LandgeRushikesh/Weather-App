b860b11885c844dbd9d20b48d73b69473fdc5799
LandgeRushikesh <rushilandge12@gmail.com>
2024-11-20 23:10:02 +0530

    search feature added

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-let cityname = "Mumbai"
-let APIKey = `916aadbc8a2241c004d71a5301fdcda5`
-let url =  `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`
 
+let APIKey = `916aadbc8a2241c004d71a5301fdcda5`
 
 let resTemp = document.querySelector(".res-temp")
 let reshumidity = document.querySelector(".res-humidity")
 let resWind = document.querySelector(".res-wind") 
 let city = document.querySelector(".city") 
-let fun = async() =>{
+let searchBar = document.querySelector(".search-bar")
+let searchBtn = document.querySelector(".search-btn")
+
+let weather = async(cityname) =>{
+    let url =  `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`
     let response = await fetch(url)
     let data = await response.json()
     console.log(data)
+
     let temp = data.main.temp
     let wind = data.wind.speed
     let humidity = data.main.humidity
@@ -18,9 +21,12 @@ let fun = async() =>{
 
     resTemp.innerText = `${temp}°C`
     city.innerText = cityname
-    reshumidity.innerText = `humidity : ${humidity}%`
+    reshumidity.innerText = `${humidity}%`
     resWind.innerText = `${wind} m/s`
 }
 
-fun()
+searchBtn.addEventListener("click",()=>{
+    let cityname = searchBar.value
+    weather(cityname)
+})
 
