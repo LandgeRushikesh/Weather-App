5aa1b20c5db0422aef8f347694d4031e5f23ffba
LandgeRushikesh <rushilandge12@gmail.com>
2024-11-21 22:36:40 +0530

    location feature added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
 let APIKey = `916aadbc8a2241c004d71a5301fdcda5`
+
 
 let resTemp = document.querySelector(".res-temp")
 let reshumidity = document.querySelector(".res-humidity")
@@ -8,8 +8,9 @@ let city = document.querySelector(".city")
 let searchBar = document.querySelector(".search-bar")
 let searchBtn = document.querySelector(".search-btn")
 let description = document.querySelector(".description")
+let app = document.querySelector(".weather-app")
 
-let weather = async(cityname) =>{
+let weatherONCityName = async(cityname) =>{
     let url =  `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`
     try {
         let response = await fetch(url)
@@ -21,24 +22,40 @@ let weather = async(cityname) =>{
         let temp = data.main.temp
         let wind = data.wind.speed
         let humidity = data.main.humidity
+        cityname = data.name
         let desc = data.weather[0].main
+        let icon = data.weather[0].icon
         if (desc === "Rain" || desc === "Drizzle" || desc === "Thunderstorm") {
-            description.innerText = `It's going to rain! Carry an umbrella.`
+            description.innerText = `It's going to rain! Carry an umbrella.`;
         } else if (desc === "Clear") {
-            description.innerText = "It's a sunny day!"
+            // Assuming you can check whether it's day or night, you can do something like:
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "It's a sunny day!";
+            } else {
+                description.innerText = "Clear skies tonight!";
+            }
         } else if (desc === "Clouds") {
-            description.innerText = "It's a cloudy day."
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "It's a cloudy day.";
+            } else {
+                description.innerText = "Cloudy night ahead.";
+            }
         } else if (desc === "Snow") {
-            description.innerText = "It's snowing! Stay warm."
-        } 
-        else if(desc === "Mist"|| desc === "Smoke" || desc === "Haze"||desc === "Dust" || desc === "Fog" || desc ==="Sand"){
-            description.innerText = " Hazy and foggy"
-        }
-        else {
-            description.innerText = `Weather condition is,${desc}`
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "It's snowing! Stay warm.";
+            } else {
+                description.innerText = "Snowfall at night! Stay cozy.";
+            }
+        } else if (desc === "Mist" || desc === "Smoke" || desc === "Haze" || desc === "Dust" || desc === "Fog" || desc === "Sand") {
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "Hazy and foggy during the day.";
+            } else {
+                description.innerText = "Hazy and foggy night.";
+            }
+        } else {
+            description.innerText = `Weather condition is, ${desc}`;
         }
 
-        cityname = data.name
     
         resTemp.innerText = `${temp}°C`
         city.innerText = cityname
@@ -49,8 +66,75 @@ let weather = async(cityname) =>{
     }
 }
 
+let getLocation = () =>{
+    navigator.geolocation.getCurrentPosition(position=>{
+        let latitude = position.coords.latitude
+        let longitude = position.coords.longitude
+        weatherONLocation(latitude,longitude)
+    })
+}
+
+let weatherONLocation = async(latitude,longitude) =>{
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKey}&units=metric`
+    try {
+        let response = await fetch(url)
+        if(!response.ok){
+            throw new Error("City Not Found!!!");
+        }
+        let data = await response.json()
+        console.log(data)
+        let temp = data.main.temp
+        let wind = data.wind.speed
+        let humidity = data.main.humidity
+        cityname = data.name
+        let desc = data.weather[0].main
+        let icon = data.weather[0].icon
+        if (desc === "Rain" || desc === "Drizzle" || desc === "Thunderstorm") {
+            description.innerText = `It's going to rain! Carry an umbrella.`;
+        } else if (desc === "Clear") {
+            // Assuming you can check whether it's day or night, you can do something like:
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "It's a sunny day!";
+            } else {
+                description.innerText = "Clear skies tonight!";
+            }
+        } else if (desc === "Clouds") {
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "It's a cloudy day.";
+            } else {
+                description.innerText = "Cloudy night ahead.";
+            }
+        } else if (desc === "Snow") {
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "It's snowing! Stay warm.";
+            } else {
+                description.innerText = "Snowfall at night! Stay cozy.";
+            }
+        } else if (desc === "Mist" || desc === "Smoke" || desc === "Haze" || desc === "Dust" || desc === "Fog" || desc === "Sand") {
+            if (icon[icon.length - 1] === "d") {
+                description.innerText = "Hazy and foggy during the day.";
+            } else {
+                description.innerText = "Hazy and foggy night.";
+            }
+        } else {
+            description.innerText = `Weather condition is, ${desc}`;
+        }
+        resTemp.innerText = `${temp}°C`
+        city.innerText = cityname
+        reshumidity.innerText = `${humidity}%`
+        resWind.innerText = `${wind} m/s`
+    } catch (error) {
+        alert("City Not Found!!")
+    }
+}
+
+window.onload= ()=>{
+    getLocation();
+    searchBar.value = ""
+}
+
 searchBtn.addEventListener("click",()=>{
     let cityname = searchBar.value
-        weather(cityname)
+        weatherONCityName(cityname)
 })
 
